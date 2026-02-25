391d13f5d60bf9ee69d806c163d92781d9060901
Hong SH <aerofleet72@gmail.com>
2026-02-25 23:04:27 +0900

    mission0220 1st commit

---
+++ backend/src/main/java/com/goorm/mission0220/src/main/java/com/goorm/mission0220/Mission0220Application.java
@@ -0,0 +1,13 @@
+package com.goorm.mission0220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission0220Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission0220Application.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/goorm/mission0220/src/test/java/com/goorm/mission0220/Mission0220ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.mission0220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission0220ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
